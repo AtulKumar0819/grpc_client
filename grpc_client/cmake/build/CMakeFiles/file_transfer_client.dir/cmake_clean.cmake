ec3bb288fecf6c76148de8388e4d51768617db4b
atul <atul.singh@innobitsystems.com>
2024-07-09 10:32:39 +0530

    grpc client in c++

---
+++ grpc_client/cmake/build/CMakeFiles/file_transfer_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/file_transfer_client.dir/file_transfer_client.cc.o"
+  "file_transfer_client"
+  "file_transfer_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/file_transfer_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
